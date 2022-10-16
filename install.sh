10711019ba110303df22292c30517c1db42d3a23
Barak Hoffer <barak.hoffer@gmail.com>
2022-10-16 10:44:41 +0300

    Improve install script

--- install.sh
+++ install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-SCRIPT_DIR="$( dirname -- "$0"; )"
+SCRIPT_DIR="$(realpath $( dirname -- "$0"; ))"
 XYCE_BIN="$(dirname -- $(which Xyce);)"
 XYCE_LIB="$(realpath "$XYCE_BIN/../lib")"
+
+if [$(which buildxyceplugin.sh)]; then
+    BUILD_SCRIPT=buildxyceplugin.sh
+elif [$(which buildxyceplugin)]; then
+    BUILD_SCRIPT=buildxyceplugin
+else
+    echo "Can not find build xyce plugin script."
+    exit 1
 
 cp "$SCRIPT_DIR/xyce/XyceLP.sh" "$XYCE_BIN"
 chmod +x "$XYCE_BIN/XyceLP.sh"
@@ -12,8 +20,8 @@ mkdir -p "$XYCE_LIB/plugins"
 git clone https://github.com/google/skywater-pdk-libs-sky130_fd_pr_reram sky130_fd_pr_reram
 cp sky130_fd_pr_reram/cells/reram_cell/sky130_fd_pr_reram__reram_cell.va $SCRIPT_DIR/xyce/sky130_fd_pr_reram__reram_module.va
 patch "$SCRIPT_DIR/xyce/sky130_fd_pr_reram__reram_module.va" < "$SCRIPT_DIR/xyce/va_model_patch"
-buildxyceplugin -o sky130_fd_pr_reram__reram_module "$SCRIPT_DIR/xyce/sky130_fd_pr_reram__reram_module.va" "$SCRIPT_DIR/build"
-cp  "$SCRIPT_DIR/build/sky130_fd_pr_reram__reram_module.so" "$XYCE_LIB/plugins"
+$BUILD_SCRIPT -o sky130_fd_pr_reram__reram_module "$SCRIPT_DIR/xyce/sky130_fd_pr_reram__reram_module.va" "$SCRIPT_DIR/build"
+cp  "$SCRIPT_DIR/build/*.so" "$XYCE_LIB/plugins"
 
 mkdir -p ${PDK_ROOT}/sky130B/libs.tech
 cp -r "$SCRIPT_DIR/xyce" ${PDK_ROOT}/sky130B/libs.tech
